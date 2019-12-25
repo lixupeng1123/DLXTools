c867adf80673258913b008a121ffb20346493bec
lxp1123 <lixupeng1123@126.com>
2019-12-25 11:02:23 +0800

    第一次提交

---
+++ DlXTools/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DlXTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public string GetString()
+        {
+            return "这个消息来自 Home 控制器下的 About 的视图文件 About.cshtml";
+        }
+    }
+}
